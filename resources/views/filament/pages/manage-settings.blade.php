daa59ca91ee869a369f211f819e652645c65db3c
DevAyrus <devayrus@gmail.com>
2026-04-30 18:20:38 +0700

    Add spacing between form and save button

--- resources/views/filament/pages/manage-settings.blade.php
+++ resources/views/filament/pages/manage-settings.blade.php
@@ -1,8 +1,8 @@
 <x-filament-panels::page>
-    <form wire:submit="save">
+    <form wire:submit="save" class="space-y-6">
         {{ $this->form }}
 
-        <div class="mt-4 flex justify-end">
+        <div class="flex flex-wrap items-center gap-4 justify-end">
             <x-filament::button type="submit">
                 Simpan Pengaturan
             </x-filament::button>
