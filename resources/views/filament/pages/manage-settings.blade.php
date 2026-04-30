62b330b609eeaf3e11b9a296f2d44433ad67c1b7
DevAyrus <devayrus@gmail.com>
2026-04-30 18:22:10 +0700

    Force spacing and add border separator for save button

--- resources/views/filament/pages/manage-settings.blade.php
+++ resources/views/filament/pages/manage-settings.blade.php
@@ -1,8 +1,8 @@
 <x-filament-panels::page>
-    <form wire:submit="save" class="space-y-6">
+    <form wire:submit="save">
         {{ $this->form }}
 
-        <div class="flex flex-wrap items-center gap-4 justify-end">
+        <div class="mt-8 pt-4 border-t border-gray-200 dark:border-white/10 flex flex-wrap items-center gap-4 justify-end">
             <x-filament::button type="submit">
                 Simpan Pengaturan
             </x-filament::button>
