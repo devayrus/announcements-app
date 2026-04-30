30127c76fe1b55687b2cd3c9e7e1cc069b750f1d
DevAyrus <devayrus@gmail.com>
2026-04-30 18:24:15 +0700

    Use inline style for spacing to avoid purged classes

--- resources/views/filament/pages/manage-settings.blade.php
+++ resources/views/filament/pages/manage-settings.blade.php
@@ -2,7 +2,7 @@
     <form wire:submit="save">
         {{ $this->form }}
 
-        <div class="mt-8 pt-4 border-t border-gray-200 dark:border-white/10 flex flex-wrap items-center gap-4 justify-end">
+        <div style="margin-top: 2rem; padding-top: 1rem;" class="border-t border-gray-200 dark:border-white/10 flex flex-wrap items-center gap-4 justify-end">
             <x-filament::button type="submit">
                 Simpan Pengaturan
             </x-filament::button>
